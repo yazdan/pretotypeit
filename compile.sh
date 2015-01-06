4aba88ae89cef5abd18176d2d5ffc89a43a09209
Abbas Yazdanpanah <yazdanpanah.a@gmail.com>
2015-01-06 08:46:03 +0330

    first commit

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#/usr/bin/sh
+for i in *.md
+do
+	pandoc -f markdown --latex-engine=xelatex -R -i "$i" -o "./latex/${i%%.*}.tex"
+	FULL_PATH=$(readlink -f $i)
+	ln -s "$FULL_PATH" "./booksite/content/"
+done
+
+cd latex
+for i in *.tex
+do
+	sed -i.bak 's/\[<+->\]//' "$i"
+	sed -i.bak 's/\\def\\labelenumi{\\arabic{enumi}.}//g' "$i"
+	sed -i.bak 's/\\itemsep1pt\\parskip0pt\\parsep0pt//g' "$i"
+	
+done
+
+xelatex -interaction=nonstopmode -synctex=-1 "doc.tex"
+
+cd ..
+
---
+++ this_is_embarrassing.md
@@ -0,0 +1,7 @@
+---
+draft: true
+title: "this is embarresing"
+date: 2014-05-20T10:04:31Z
+---
+قسمت اول
+======================

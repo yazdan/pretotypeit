0b5b51e9df73141ba5aff9d11c02c3687aed43ad
Abbas Yazdanpanah <yazdanpanah.a@gmail.com>
2015-10-24 11:38:31 +0330

    - hugo 0.14 is addopted

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
 #/usr/bin/sh
+rm -f ./booksite/content/*
 for i in *.md
 do
 	pandoc -f markdown+pipe_tables+simple_tables+table_captions --latex-engine=xelatex -R -i "$i" -o "./latex/${i%%.*}.tex" --filter ./pandoc-xepersian/pandoc-xepersian.py
 	FULL_PATH=$(readlink -f $i)
-	ln -s "$FULL_PATH" "./booksite/content/"
+	cp "$FULL_PATH" "./booksite/content/"
 done
 
 cd latex
@@ -18,6 +19,4 @@ done
 xelatex -interaction=nonstopmode -synctex=-1 "doc.tex"
 
 cd ..
-
-pandoc -t epub3 -S -o pretotypeit.epub --epub-embed-font='./epub/IRANSans-Bold-web.ttf' --epub-embed-font='./epub/IRANSans-Light-web.ttf' --epub-embed-font='./epub/IRANSans-Medium-web.ttf' --epub-embed-font='./epub/IRANSans-UltraLight-web.ttf' --epub-embed-font='./epub/IRANSans-web.ttf' --epub-stylesheet=./epub/epub.css ./epub/title.txt this_is_embarrassing.md introduction.md translator_introduction.md the_right_it.md pretotyping.md it_will_fail.md pretotype_it.md test_it.md put_it_all_together.md now_go_make_it.md bonus_feature.md pretotyping_manifesto.md about_the_author.md acknowledgments.md follow_pretotyping.md 
 
